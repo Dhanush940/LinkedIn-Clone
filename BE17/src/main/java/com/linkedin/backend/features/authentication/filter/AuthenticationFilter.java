b8b23c678f7873be431377ffadd48499768c8981
Dhanush940 <dhanushkunamaneni123@gmail.com>
2025-09-10 20:17:31 +0530

    feat (backend): add file uploading and streaming capability

--- BE17/src/main/java/com/linkedin/backend/features/authentication/filter/AuthenticationFilter.java
+++ BE17/src/main/java/com/linkedin/backend/features/authentication/filter/AuthenticationFilter.java
@@ -8,8 +8,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -18,9 +16,6 @@ import java.util.List;
 
 @Component
 public class AuthenticationFilter extends HttpFilter {
-    @Value("${frontend.url}")
-    private String frontendUrl;
-
     private final List<String> unsecuredEndpoints = Arrays.asList(
             "/api/v1/authentication/login",
             "/api/v1/authentication/register",
@@ -38,7 +33,7 @@ public class AuthenticationFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        response.addHeader("Access-Control-Allow-Origin", frontendUrl);
+        response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
@@ -49,11 +44,11 @@ public class AuthenticationFilter extends HttpFilter {
 
         String path = request.getRequestURI();
 
-        if (unsecuredEndpoints.contains(path) || path.startsWith("/api/v1/authentication/oauth")) {
+        if (unsecuredEndpoints.contains(path) || path.startsWith("/api/v1/authentication/oauth") || path.startsWith("/api/v1/storage")) {
             chain.doFilter(request, response);
             return;
         }
-        
+
         try {
             String authorization = request.getHeader("Authorization");
             if (authorization == null || !authorization.startsWith("Bearer ")) {
